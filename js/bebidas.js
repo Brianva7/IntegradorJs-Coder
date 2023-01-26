5c6c2fee807b442c65c2aa75f8c0fbc1b4344f08
Brian Vazques <vazquesbrian7@gmail.com>
2023-01-26 04:57:27 -0300

    v0.4

--- js/bebidas.js
+++ js/bebidas.js
@@ -2,57 +2,56 @@
 
 const verBebidas = document.querySelector("#bebidas");
 
+const mostrarBebidas = async () => {
+  listaProductos.innerHTML = "";
+  const resp = await fetch("../megasabrosos");
+  const data = await resp.json();
+
+  data.forEach((post) => {
+    const producto = document.createElement("p");
+    producto.innerHTML = `
+    <div class="mx-2">
+    <img src="./img/hamburguesa.jpg" class="card-img-top" alt="card-grid-image">
+    <div class="card-body">
+    <h5 class="card-title">${post.nombre}</h5>
+    <p class="card-text">$${post.precio}<p>
+    </div>
+    </div>`;
+
+    listaProductos.append(producto);
+
+    const addBtn = document.createElement("button");
+    addBtn.innerText = "agregar";
+    addBtn.className = "btn btn-dark m-2";
+
+    producto.append(addBtn);
+
+    addBtn.addEventListener("click", () => {
+      const r = carrito.some((r) => r.id === e.id);
+      if (r) {
+        carrito.map((a) => {
+          if (a.id === e.id) {
+            a.cantidad++;
+          }
+        });
+      } else {
+        carrito.push({
+          id: e.id,
+          nombre: e.nombre,
+          precio: e.precio,
+          toppings: e.toppings,
+          cantidad: e.cantidad,
+        });
+      }
+
+      guardarCarrito();
+      verCarrito();
+    });
+  });
+};
+
 verBebidas.addEventListener("click", () => {
   mostrarBebidas();
 });
-
-const mostrarBebidas = () => {
-  listaProductos.innerHTML = "";
-  fetch(`./bebidas.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((e) => {
-        const producto = document.createElement("p");
-        producto.innerHTML = `
-        <div class="mx-2">
-        <img src="./img/hamburguesa.jpg" class="card-img-top" alt="card-grid-image">
-        <div class="card-body">
-        <h5 class="card-title">${e.nombre}</h5>
-      <p class="card-text">$${e.precio}<p>
-      </div>
-      </div>`;
-
-        listaProductos.append(producto);
-
-        const addBtn = document.createElement("button");
-        addBtn.innerText = "agregar";
-        addBtn.className = "btn btn-dark m-2";
-
-        producto.append(addBtn);
-
-        addBtn.addEventListener("click", () => {
-          const r = carrito.some((r) => r.id === e.id);
-          if (r) {
-            carrito.map((a) => {
-              if (a.id === e.id) {
-                a.cantidad++;
-              }
-            });
-          } else {
-            carrito.push({
-              id: e.id,
-              nombre: e.nombre,
-              precio: e.precio,
-              toppings: e.toppings,
-              cantidad: e.cantidad,
-            });
-          }
-
-          guardarCarrito();
-          verCarrito();
-        });
-      });
-    });
-};
 
 // mostrar seccion de productos "Bebidas"
